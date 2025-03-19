95fbd9fd6256a50775b92b41d2269c1f8df7cf8a
xxdakexx28 <bajormi.2807@gmail.com>
2025-03-19 17:35:40 -0400

    firts commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+@app.get("/")
+def hello_world():
+    return {"message": "Hello World"}
+
+
+class AgenteBasico(BaseModel):
+    prompt: str
+
+@app.post("/agente-basico")
+async def agente_basico(prompt: AgenteBasico):
+    return {
+        "prompt": prompt.prompt, 
+        }
